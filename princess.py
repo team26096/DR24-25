0099e56b92f7d15c2ad8f068c23c6d3eb34a7278
team26096 <115370317+team26096@users.noreply.github.com>
2024-10-26 19:12:12 -0400

    Update princess.py
    Coded run 6 to complete Sonar Discovery but we still need to fix coming back to base.

--- princess.py
+++ princess.py
@@ -198,70 +198,99 @@ async def run6():
     # # go forward to to get out of base and go towards feed the whale
     motor.reset_relative_position(port.A, 0)
     initial_position = abs(motor.relative_position(port.A))
-    # await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=300, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
-    #                 initial_position=initial_position, distance_to_cover=(degreesForDistance(81)))
+    await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=538, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+                    initial_position=initial_position, distance_to_cover=(degreesForDistance(75)))
 
-    # # turn right to align with feed the whale
-    # await pivot_gyro_turn_abs(50, -50, 40, True)
+    # turn right to align with feed the whale
+    await pivot_gyro_turn_abs(65, -65, 45, True)
 
-    # # move forward to open whale's mouth
-    # await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(10), 0, velocity=175)
+    # move forward to open whale's mouth
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(12), 0, velocity=175)
 
-    # # turn motor to move food tray down
-    # await motor.run_for_degrees(port.C, 720, 800)
+    # turn motor to move food tray down
+    await motor.run_for_degrees(port.B, 1080, 850)
 
-    # # move motor to lift food tray so it does not make whale vomit while coming back
-    # await motor.run_for_degrees(port.C, 720, -800)
+    # move motor to lift food tray so it does not make whale vomit while coming back
+    await motor.run_for_degrees(port.B, 720, -800)
 
-    # Move robot back to move away from feed the whale
+    # move robot back to move away from feed the whale
+    motor.reset_relative_position(port.A, 0)
+    motion_sensor.set_yaw_face(motion_sensor.TOP)
+    motion_sensor.reset_yaw(0)
+    initial_position = abs(motor.relative_position(port.A))
+    await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=-190, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+        initial_position=initial_position, distance_to_cover=degreesForDistance(23))
+
+    # turn robot to align for Sonar Discovery
+    await pivot_gyro_turn_abs(80, -80, 138, True)
+
+    # move robot back to complete alignment with Sonar Discovery
+    motor.reset_relative_position(port.A, 0)
+    initial_position = abs(motor.relative_position(port.A))
+    await follow_gyro_angle(kp=1.45, ki=0, kd=0, speed=-190, target_angle=138, sleep_time=0, follow_for=follow_for_distance,
+        initial_position=initial_position, distance_to_cover=degreesForDistance(39))
+
+    # turn Sonar Discovery attachment motor to complete Sonar Discovery
+    await motor.run_for_degrees(port.C, 360, -250)
+
+    # turn attachment other way so it does not get stuck
+    await motor.run_for_degrees(port.C, -150, -250)
+
+    # move forward to come back to base
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(50), 0, velocity=175)
+
+    # turn robot to go into base without being out
+    await pivot_gyro_turn_abs(-80, 80, 60, True)
+
+    # move forward to go and end in base
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(30), 0, velocity=175)
+
+
+
+
+
+    # # Start going to Sonar Discovery
+    # motor.reset_relative_position(port.A, 0)
+
+    # await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=-300, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+    #                initial_position=initial_position, distance_to_cover=(degreesForDistance(11.5)))
+
+    # # Turn robot to align with sonar discovery
+    # motion_sensor.set_yaw_face(motion_sensor.TOP)
+    # motion_sensor.reset_yaw(0)
+    # await pivot_gyro_turn_abs(0, -150, 50, True)
+
+    # # Go reverse past Sonar Discovery
+    # motion_sensor.set_yaw_face(motion_sensor.TOP)
+    # motion_sensor.reset_yaw(0)
     # motor.reset_relative_position(port.A, 0)
     # initial_position = abs(motor.relative_position(port.A))
-    # await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=-175, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
-    #     initial_position=initial_position, distance_to_cover=degreesForDistance(8))
-
-
-    # Start going to Sonar Discovery
-    motor.reset_relative_position(port.A, 0)
-
-    await follow_gyro_angle(kp=-1.45, ki=0, kd=0, speed=-300, target_angle=0, sleep_time=0, follow_for=follow_for_distance,
-                   initial_position=initial_position, distance_to_cover=(degreesForDistance(11.5)))
-
-    # Turn robot to align with sonar discovery
-    motion_sensor.set_yaw_face(motion_sensor.TOP)
-    motion_sensor.reset_yaw(0)
-    await pivot_gyro_turn_abs(0, -150, 50, True)
-
-    # Go reverse past Sonar Discovery
-    motion_sensor.set_yaw_face(motion_sensor.TOP)
-    motion_sensor.reset_yaw(0)
-    motor.reset_relative_position(port.A, 0)
-    initial_position = abs(motor.relative_position(port.A))
-    distance = -65
-    await follow_gyro_angle(kp=-1.25*(int(distance/abs(distance))), ki=0.002, kd=-0.001, speed=350*(int(distance/abs(distance))), target_angle=0, sleep_time=0, follow_for=follow_for_distance,
-                    initial_position=initial_position, distance_to_cover=(degreesForDistance(distance)))
+    # distance = -65
+    # await follow_gyro_angle(kp=-1.25*(int(distance/abs(distance))), ki=0.002, kd=-0.001, speed=350*(int(distance/abs(distance))), target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+    #                 initial_position=initial_position, distance_to_cover=(degreesForDistance(distance)))
 
     # Turn left slightly
-    await turn_left(angle=-15)
+    # await turn_left(angle=-15)
 
-    # Go forward towards Sonar Discovery mission
-    motion_sensor.set_yaw_face(motion_sensor.TOP)
-    motion_sensor.reset_yaw(0)
-    motor.reset_relative_position(port.A, 0)
-    initial_position = abs(motor.relative_position(port.A))
-    distance = 13
-    await follow_gyro_angle(kp=-1.25*(int(distance/abs(distance))), ki=0.002, kd=-0.001, speed=350*(int(distance/abs(distance))), target_angle=0, sleep_time=0, follow_for=follow_for_distance,
-                    initial_position=initial_position, distance_to_cover=(degreesForDistance(distance)))
+    # # Go forward towards Sonar Discovery mission
+    # motion_sensor.set_yaw_face(motion_sensor.TOP)
+    # motion_sensor.reset_yaw(0)
+    # motor.reset_relative_position(port.A, 0)
+    # initial_position = abs(motor.relative_position(port.A))
+    # distance = 13
+    # await follow_gyro_angle(kp=-1.25*(int(distance/abs(distance))), ki=0.002, kd=-0.001, speed=350*(int(distance/abs(distance))), target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+    #                 initial_position=initial_position, distance_to_cover=(degreesForDistance(distance)))
     
     # Turn right slightly to align with Sonar Discovery mission
-    await turn_right(speed=100, angle=13)
+    # await turn_right(speed=100, angle=13)
 
-    motion_sensor.set_yaw_face(motion_sensor.TOP)
-    motion_sensor.reset_yaw(0)
-    motor.reset_relative_position(port.A, 0)
-    initial_position = abs(motor.relative_position(port.A))
-    distance = 17
-    await follow_gyro_angle(kp=-1.25*(int(distance/abs(distance))), ki=0.002, kd=-0.001, speed=350*(int(distance/abs(distance))), target_angle=0, sleep_time=0, follow_for=follow_for_distance,
-                    initial_position=initial_position, distance_to_cover=(degreesForDistance(distance)))
+    # motion_sensor.set_yaw_face(motion_sensor.TOP)
+    # motion_sensor.reset_yaw(0)
+    # motor.reset_relative_position(port.A, 0)
+    # initial_position = abs(motor.relative_position(port.A))
+    # distance = 17
+    # await follow_gyro_angle(kp=-1.25*(int(distance/abs(distance))), ki=0.002, kd=-0.001, speed=350*(int(distance/abs(distance))), target_angle=0, sleep_time=0, follow_for=follow_for_distance,
+    #                 initial_position=initial_position, distance_to_cover=(degreesForDistance(distance)))
 
     # Move robot back to get ready to complete sonar discorvery
     # motor.reset_relative_position(port.A, 0)
